cb43042e71e71221b25c4f79d9418c89be281cc6
joex2much <joex2much@gmail.com>
2023-11-06 23:02:04 -0800

    additional validations

--- practice_site/main.js
+++ practice_site/main.js
@@ -1,13 +1,40 @@
 const firstName = document.querySelector("#first-name");
-let regexText = /^[a-zA-Z]+$/;
+let regexName = /^[a-zA-Z]+$/;
+let regexEmail = /^.+@.+\..+$/;
 
 firstName.addEventListener("blur", (e) => {
   e.preventDefault();
   //   console.log(firstName.value);
-  if (!regexText.test(firstName)) {
+  if (!regexName.test(firstName.value)) {
     firstName.style.border = "2px solid #D36369";
   } else {
-    firstName.style.background = "white";
-    firstName.style.remove.border;
+    // firstName.style.background = "white";
+    firstName.style.border = "none";
+  }
+});
+
+const lastName = document.querySelector("#last-name");
+
+lastName.addEventListener("blur", (e) => {
+  e.preventDefault();
+  //   console.log(firstName.value);
+  if (!regexName.test(lastName.value)) {
+    lastName.style.border = "2px solid #D36369";
+  } else {
+    // firstName.style.background = "white";
+    lastName.style.border = "none";
+  }
+});
+
+const email = document.querySelector("#email");
+
+email.addEventListener("blur", (e) => {
+  e.preventDefault();
+  //   console.log(firstName.value);
+  if (!regexEmail.test(email.value)) {
+    email.style.border = "2px solid #D36369";
+  } else {
+    // firstName.style.background = "white";
+    email.style.border = "none";
   }
 });
